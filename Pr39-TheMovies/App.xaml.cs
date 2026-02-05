4ea30db62266103ce734ae39f705ecdf5ee6e2a7
Corinna Haustrup <corinnahaustrup@gmail.com>
2026-02-05 09:54:41 +0100

    Add project files.

---
+++ Pr39-TheMovies/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Pr39_TheMovies
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
