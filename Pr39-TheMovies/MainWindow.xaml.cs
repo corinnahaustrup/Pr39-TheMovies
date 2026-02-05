d11aea7f112bed90d728f46e8a3f540606d3008b
Corinna Haustrup <corinnahaustrup@gmail.com>
2026-02-05 11:37:25 +0100

    ny save

--- Pr39-TheMovies/MainWindow.xaml.cs
+++ Pr39-TheMovies/MainWindow.xaml.cs
@@ -20,15 +20,6 @@ namespace Pr39_TheMovies
         {
             InitializeComponent();
         }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
+       
     }
 }
